a8f0c631297dd8dd69edc48b7297b3ca87b50abb
Thasleema <thasleemapk133@gmail.com>
2026-05-17 13:56:57 +0530

    category route added

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,13 +1,60 @@
-import express from "express"
+import express from "express";
+
+import authMiddleware
+from "../middlewares/authMiddleware.js";
+
+import {
+  createCategory,
+  getCategories,
+  getSingleCategory,
+  updateCategory,
+  deleteCategory,
+} from "../controllers/categoryController.js";
 
 
-const categoryRoute = express.Router()
+const categoryRoute =
+  express.Router()
 
-categoryRoute.post("/")
-categoryRoute.get("/categories")
-categoryRoute.get("/:id",)
-categoryRoute.put("/:id",)
-categoryRoute.delete("/:id")
+
+
+categoryRoute.post(
+  "/",
+  authMiddleware,
+  createCategory
+)
+
+
+
+categoryRoute.get(
+  "/",
+  authMiddleware,
+  getCategories
+)
+
+
+
+categoryRoute.get(
+  "/:id",
+  authMiddleware,
+  getSingleCategory
+)
+
+
+
+categoryRoute.put(
+  "/:id",
+  authMiddleware,
+  updateCategory
+)
+
+
+
+categoryRoute.delete(
+  "/:id",
+  authMiddleware,
+  deleteCategory
+)
+
 
 
 export default categoryRoute
