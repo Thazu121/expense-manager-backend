b611626f5d31c5caf5b6e9a7f9ef37229a8b1106
Thasleema <thasleemapk133@gmail.com>
2026-06-11 16:58:36 +0530

    error in route corrected

--- routes/scanRoute.js
+++ routes/scanRoute.js
@@ -1,11 +1,13 @@
 import express from "express";
-import  upload  from "../middlewares/uploadMiddleware.js";
+import upload from "../middlewares/uploadMiddleware.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 import { scanReceiptController } from "../controllers/scanController.js";
 
 const scanRouter = express.Router();
 
 scanRouter.post(
   "/receipt",
+  authMiddleware,
   upload.single("receipt"),
   scanReceiptController
 );
